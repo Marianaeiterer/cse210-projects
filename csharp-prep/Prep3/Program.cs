67f638e33214d04e819dc5a0c3c1bb906d8ba45a
marieiterer@gmail.com <marieiterer@gmail.com>
2024-03-01 08:44:58 -0700

    Finished Prep 3 and Stretch Challenges

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,48 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
+    {       
+        Random randomGenerator = new Random();
+        int magicNumber;
+        int guess;
+        int numberOfGuesses;
+
+        string wantPlayAgain = "yes";
+
+        while(wantPlayAgain != "no"){
+            guess = -1;
+            numberOfGuesses = 0;
+            magicNumber = randomGenerator.Next(1, 101);
+
+            while(guess != magicNumber){
+            
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                
+                numberOfGuesses++;
+
+                if(guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if(guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+
+            }
+         
+            Console.WriteLine($"You guessed {numberOfGuesses} times");
+            Console.Write("Do you want to play again (yes or no)? ");
+            wantPlayAgain = Console.ReadLine();
+
+        
+        }
+
         
     }
 }
