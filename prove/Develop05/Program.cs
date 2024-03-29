efc4938b474aa7c0bac7d23f9fc3b1951bf1a4af
marieiterer@gmail.com <marieiterer@gmail.com>
2024-03-29 11:08:59 -0600

    exceed the core requirements

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,5 +1,6 @@
 using System;
-
+// Created a bad habit goal category where the user loses points if he does that bad habit
+// and loses a bonus points if he does as many times as he sets it. 
 class Program
 {
     static void Main(string[] args)
